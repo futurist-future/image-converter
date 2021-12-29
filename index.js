a41f4b634159116c2d6f5bb6393cd0ed13d821a4
gregoriomario <74748784+gregoriomario@users.noreply.github.com>
2021-12-29 20:04:02 +0700

    sephia start

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-const convert = require("./lib/converter");
 const path = require("path");
+const fs = require("fs");
+const convert = require("./lib/converter");
 
 const imagePath = path.join(__dirname, "bocil.bmp");
-convert.bmp.toBnW(imagePath);
+convert.bmp.toSephia(imagePath);

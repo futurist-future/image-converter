99f53c171c81c6dc2292e7cd9296a601406b512c
gregoriomario <74748784+gregoriomario@users.noreply.github.com>
2021-12-29 14:13:29 +0700

    new Class into functional

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
-const Convert = require("./lib/converter");
+const convert = require("./lib/converter");
 const path = require("path");
 
 const imagePath = path.join(__dirname, "bocil.bmp");
-const image = new Convert(imagePath);
-
-image.bmp.toBnW()
+convert.bmp.toBnW(imagePath);

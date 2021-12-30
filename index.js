124778e6ae7ef187384ce35a5a02ad3d20f9998b
gregoriomario <74748784+gregoriomario@users.noreply.github.com>
2021-12-30 19:26:09 +0700

    documentation, structure, error handler
    Several things happen to be added to this commit
    1. Imporved documentation, user can now understand the library better
    2. Structure of the class improved, removing boilerplates and wrap those in 2 new function
    3. Handle error if there is happen to be one loll

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ const path = require("path");
 const convert = require("./lib/converter");
 
 const imagePath = path.join(__dirname, "images", "bocil.bmp");
-convert.bmp.toSephia(imagePath);
+convert.bmp.toBnW(imagePath)
 

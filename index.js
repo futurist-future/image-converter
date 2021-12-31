c075ce4db4cf9e6234d9a95a2ca2141ffb8fa0ff
gregoriomario <74748784+gregoriomario@users.noreply.github.com>
2021-12-31 20:58:49 +0700

    all done kecuali sephia 24

--- index.js
+++ index.js
@@ -4,13 +4,6 @@ const convert = require("./lib/converter");
 const { splitIntoPart } = require("./lib/utils/splitPerIndex");
 const { decimalToHex } = require("./lib/utils/hexConverter");
 
-const imagePath = path.join(__dirname, "images", "sike.bmp");
+const imagePath = path.join(__dirname, "images", "place24.bmp");
 convert.bmp.toSephia(imagePath);
 
-// console.log(decimalToHex(16));
-// console.log(splitIntoPart([1, 2, 3, 4, 5, 6, 7, 8, 9,  10], 4));
-
-// See inside image
-// const imageData = fs.readFileSync(imagePath, { encoding: "hex" });
-// const outputpath = path.join(__dirname, "place32.txt");
-// fs.writeFileSync(outputpath, imageData, { encoding: "utf-8" });

0dc79056adfa926bd63f021bccacc670c0e07146
gregoriomario <74748784+gregoriomario@users.noreply.github.com>
2021-12-31 20:35:31 +0700

    black and white 32 24 bisa!

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const path = require("path");
 const fs = require("fs");
 const convert = require("./lib/converter");
 const { splitIntoPart } = require("./lib/utils/splitPerIndex");
+const { decimalToHex } = require("./lib/utils/hexConverter");
 
-const imagePath = path.join(__dirname, "images", "place24.bmp");
-convert.bmp.toBnW(imagePath);
+const imagePath = path.join(__dirname, "images", "sike.bmp");
+convert.bmp.toSephia(imagePath);
 
+// console.log(decimalToHex(16));
 // console.log(splitIntoPart([1, 2, 3, 4, 5, 6, 7, 8, 9,  10], 4));
 
 // See inside image

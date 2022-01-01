8499776b87038fff4d48f6288e82c7558b931cce
gregoriomario <74748784+gregoriomario@users.noreply.github.com>
2022-01-01 22:26:39 +0700

    blur buat image2 depth 32

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
+const fs = require("fs");
 const path = require("path");
 const convert = require("./lib/converter");
 const { splitPerIndex } = require("./lib/utils/splitPerIndex");
 
-const imagePath = path.join(__dirname, "images", "sike.bmp");
-convert.bmp.toSephia(imagePath);
+const imagePath = path.join(__dirname, "images", "7.bmp");
+convert.bmp.blur(imagePath);
+
+//const outputPath = path.join(__dirname, "7.txt");
+
+// const imageData = fs.readFileSync(imagePath, { encoding: "hex" });
+// fs.writeFileSync(outputPath, imageData, { encoding: "utf-8" });

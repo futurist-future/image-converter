063e3383fddca114e2b2e800ee3b965e44da54a2
gregoriomario <74748784+gregoriomario@users.noreply.github.com>
2022-01-01 17:17:22 +0700

    reflect 24 bisa dan perubahan struktur
    struktur pembagian file diubah lagi karena ga consider padding sebelumnya
    karena di 24 bit padding pengaruh banget, jadinya file dibagi dulu berdasarkan heightnya baru ketauan paddingnya berapa deh

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const convert = require("./lib/converter");
+const { splitPerIndex } = require("./lib/utils/splitPerIndex");
 
-const imagePath = path.join(__dirname, "images", "bocil.bmp");
-convert.bmp.reflect(imagePath);
+const imagePath = path.join(__dirname, "images", "sike.bmp");
+convert.bmp.toSephia(imagePath);

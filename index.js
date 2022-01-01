9fdc89aae0d561377352af26c905400774f85677
Greg <mistzero0@gmail.com>
2022-01-01 15:40:07 +0700

    image reflected

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
 const path = require("path");
-const fs = require("fs");
 const convert = require("./lib/converter");
-const { splitIntoPart } = require("./lib/utils/splitPerIndex");
-const { decimalToHex } = require("./lib/utils/hexConverter");
 
-const imagePath = path.join(__dirname, "images", "place24.bmp");
-convert.bmp.toSephia(imagePath);
-
+const imagePath = path.join(__dirname, "images", "bocil.bmp");
+convert.bmp.reflect(imagePath);

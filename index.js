f5045f41804b07752f67a6fd9896cabffdb2c0e4
gregoriomario <74748784+gregoriomario@users.noreply.github.com>
2022-01-03 17:13:48 +0700

    edge 24 working

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const path = require("path");
 const convert = require("./lib/converter");
 const { splitPerIndex } = require("./lib/utils/splitPerIndex");
 
-const imagePath = path.join(__dirname, "images", "bocil.bmp");
-convert.bmp.blur(imagePath, "light");
+const imagePath = path.join(__dirname, "images", "place24.bmp");
+convert.bmp.edge(imagePath, "light");
 
 //const outputPath = path.join(__dirname, "7.txt");
 
